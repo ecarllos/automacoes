7179bbe3b73ae717a1f56e070c3bf9ecfa8d9dc6
ecarllos <ecarllos017@gmail.com>
2025-08-07 14:52:20 -0300

    tamanho tela (funcoesPyautogui)

--- EmpresasCEP/funcoesPyautogui.py
+++ EmpresasCEP/funcoesPyautogui.py
@@ -2,6 +2,6 @@ import pyautogui
 
 pyautogui.PAUSE = 3 # Define que a cada ação terá um intervalo de 3 segundos de uma para outra
 print(pyautogui.position()) # Pegar a posição do mouse
-print(pyautogui.size()) # Pegar o tamanho da tela
+print(pyautogui.size()) # Pegar o tamanho da tela        1920x1080
 pyautogui.click(x=500, y=500) # Clicar com o mouse
 pyautogui.moveTo(x=345, y=902, duration = 1) # Mover o mouse
