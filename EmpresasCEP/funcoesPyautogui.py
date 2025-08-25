be427a94f1fa9b5b52d9b98ddee2081fd3610f40
ecarllos <ecarllos017@gmail.com>
2025-08-25 15:30:20 -0300

    create automacoes in work

--- EmpresasCEP/funcoesPyautogui.py
+++ EmpresasCEP/funcoesPyautogui.py
@@ -1,7 +1,16 @@
 import pyautogui
+import time
 
-pyautogui.PAUSE = 3 # Define que a cada ação terá um intervalo de 3 segundos de uma para outra
-print(pyautogui.position()) # Pegar a posição do mouse
-print(pyautogui.size()) # Pegar o tamanho da tela        1920x1080
-pyautogui.click(x=500, y=500) # Clicar com o mouse
-pyautogui.moveTo(x=345, y=902, duration = 1) # Mover o mouse
+time.sleep(3) # Espera 2 segundos para fazer alguma ação
+pyautogui.PAUSE = 1 # Define que a cada ação terá um intervalo de 3 segundos de uma para outra
+# print(pyautogui.position()) # Pegar a posição do mouse
+# print(pyautogui.size()) # Pegar o tamanho da tela        1920x1080
+
+# #Funções do Mouse
+# pyautogui.click(x=500, y=500) # Clicar com o mouse
+# pyautogui.moveTo(x=345, y=902, duration = 1) # Mover o mouse
+
+# #Funções do Teclado
+# pyautogui.write("Eu amo minha namorada") # escreve na tela 
+# pyautogui.hotkey("Alt", "Tab") # Clica em mais de uma tecla
+# pyautogui.press("win") # Pressionar alguma tecla
--- EmpresasCEP/index.py
+++ EmpresasCEP/index.py
@@ -2,5 +2,13 @@ import pyautogui
 import time
 
 
-time.sleep(3)
-pyautogui.click(x=368, y=344)
+time.sleep(5)
+# print(pyautogui.position())
+
+# pyautogui.click(x=317, y=656) # Posição da 1° empresa
+pyautogui.press("down")
+pyautogui.hotkey("ctrl" + "c")
+
+time.sleep(5)
+
+pyautogui.hotkey("ctrl" + "v")
