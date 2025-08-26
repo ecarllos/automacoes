91208843df234c8678959f3b1a96b87631e5a3c7
Eduardo <ecarllos017@gmail.com>
2025-08-25 23:09:15 -0300

    erro que estava barrando o processo foi concertado, troca de + por ,

--- EmpresasCEP/index.py
+++ EmpresasCEP/index.py
@@ -1,14 +1,15 @@
 import pyautogui
 import time
 
+pyautogui.PAUSE = 2
+time.sleep(4)
+print(pyautogui.position())
 
-time.sleep(5)
-# print(pyautogui.position())
 
 # pyautogui.click(x=317, y=656) # Posição da 1° empresa
-pyautogui.press("down")
-pyautogui.hotkey("ctrl" + "c")
+# pyautogui.press("down")
+pyautogui.hotkey("ctrl" , "c")
 
-time.sleep(5)
-
-pyautogui.hotkey("ctrl" + "v")
+time.sleep(3)
+print(pyautogui.position())
+pyautogui.hotkey("ctrl" , "v")
