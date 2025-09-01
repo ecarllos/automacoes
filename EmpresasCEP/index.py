067fca71a7ee859b0130fb29d3e780e6ae1dea5f
ecarllos <ecarllos017@gmail.com>
2025-09-01 10:20:07 -0300

    feat: Permitir interrupção da execução com ESC

--- EmpresasCEP/index.py
+++ EmpresasCEP/index.py
@@ -1,40 +1,53 @@
+# Esse código tem a função de concertar os CNPJ's da planilha de relacionamento e alterar o consultor da empresa do SIstema
+
 import pyautogui
 import time
+import keyboard
+import sys
 
 pyautogui.PAUSE = 1.5
-time.sleep(5)
+time.sleep(4)
 
-# PEGAR O CÓDIGO NA PLANILHA
-pyautogui.press("down") # Abaixar para ir para célula do código da empresa
-pyautogui.hotkey("ctrl" , "c") # Copiar o código
-pyautogui.hotkey("ctrl" , "tab") # Mudar para página do Hapvida
+while True:
+    # PEGAR O CÓDIGO NA PLANILHA
+    pyautogui.hotkey("down","right","right","right","right") # Abaixar para ir para célula do código da empresa
+    pyautogui.hotkey("ctrl" , "c") # Copiar o código
+    pyautogui.hotkey("alt" , "tab") # Mudar para página do Hapvida
 
-time.sleep(3)
+    time.sleep(2)
 
-# LOGAR NO HAPVIDA
+    # LOGAR NO HAPVIDA
 
-pyautogui.press(["tab","tab","tab","tab","tab","tab","tab"], interval=0.5) # Selecionar o label do código
-pyautogui.hotkey("ctrl" , "v") # Cola o código copiado na tela anterior
-pyautogui.press("tab") # Vai pro campo de senha
-pyautogui.press("tab")
-pyautogui.write("197547") # Senha do SEC
-pyautogui.press(["tab","tab"]) # Vai pro campo de Enviar
-pyautogui.press("enter")
+    pyautogui.press(["tab","tab","tab","tab","tab","tab","tab"], interval=0.5) # Selecionar o label do código
+    pyautogui.hotkey("ctrl" , "v") # Cola o código copiado na tela anterior
+    pyautogui.press("tab") # Vai pro campo de senha
+    pyautogui.press("tab")
+    pyautogui.write("197547") # Senha do SEC
+    pyautogui.press(["tab","tab"]) # Vai pro campo de Enviar
+    pyautogui.press("enter")
 
-time.sleep(6) # Pause para eu clicar no botão de enviar
+    time.sleep(6) # Pause para eu clicar no botão de enviar
 
-# PEGAR O CNPJ
-pyautogui.click(x=1311, y=264) # Abrir a parte de informações da empresa
-pyautogui.doubleClick(x=932, y=306) # Selecionar o CNPJ
-pyautogui.hotkey("ctrl" , "c") # Copiar o CNPJ
-pyautogui.click(x=1290, y=217) # Sair da seção 
+    # PEGAR O CNPJ
+    # pyautogui.click(x=1311, y=264) # Abrir a parte de informações da empresa
+    # pyautogui.doubleClick(x=932, y=306) # Selecionar o CNPJ
 
-pyautogui.hotkey("ctrl" , "tab") # Mudar para página do Simplifica
+    pyautogui.hotkey("ctrl" , "c") # Copiar o CNPJ
+
+    # pyautogui.click(x=1290, y=217) # Sair da seção 
+
+    pyautogui.click(x=757, y=62) # Clicar no campo de pesquisa para confirmar que foi feito o (Ctrl + C)
+
+    pyautogui.hotkey("ctrl" , "tab") # Mudar para página do Simplifica
 
 
-# PESQUISAR A EMPRESA
-pyautogui.click(x=1622, y=233) # Selecionar a parte do CNPJ
-pyautogui.hotkey("ctrl" , "a") # Selecionar o CNPJ antigo que foi deixado
-pyautogui.hotkey("ctrl" , "v") # Colar o CNPJ
-pyautogui.press("enter") # Procurar o cnpj
+    # PESQUISAR A EMPRESA
+    pyautogui.click(x=1622, y=233) # Selecionar a parte do CNPJ
+    pyautogui.hotkey("ctrl" , "a") # Selecionar o CNPJ antigo que foi deixado
+    pyautogui.hotkey("ctrl" , "v") # Colar o CNPJ
+    pyautogui.press("enter") # Procurar o cnpj
 
+
+
+    if keyboard.is_pressed('esc'): # Determina o "fim" da função
+        sys.exit()
