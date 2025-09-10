554efc1ef5c00ba79b6e53e16396c34e0611fa7a
ecarllos <ecarllos017@gmail.com>
2025-09-10 15:00:09 -0300

    feat sistema simplifica

--- Selenium/commands.py
+++ Selenium/commands.py
@@ -1,46 +1,57 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 # importar o navegador
 navegador = webdriver.Chrome()
 
 # acessar um site
-navegador.get("https://app.simplificagestao.com.br/ords/r/api/simplifica/login_desktop ")
+navegador.get("https://youtube.com")
 
 # colocar o navegador em tela cheia
 navegador.maximize_window()
 
 # pausa
-time.sleep(10)
+# time.sleep(10)
 
 # selecionar elementos na tela
-botao_logar = navegador.find_element(By.ID, "P101_LOGIN")
+botao_pesquisa = navegador.find_element(By.NAME, "search_query")
 
 # Clicar 
-botao_logar.click()
+botao_pesquisa.click()
+
+# Escrever
+botao_pesquisa.send_keys("surf medina")
+
+# Enter
+botao_pesquisa.send_keys(Keys.ENTER)
+
+time.sleep(4)
 
 # encontrar vários elementos
-listar_botoes = navegador.find_elements(By.ID, "P101_LOGIN")
+listarAbasInternas = navegador.find_elements(By.CSS_SELECTOR, ".yt-simple-endpoint.style-scope.ytd-mini-guide-entry-renderer")
+inicio = navegador.find_element(By.ID, "endpoint")
+# inicio.click()
 
-for botao in listar_botoes:
-    if "Nome" in botao.text:
+for botao in listarAbasInternas:
+    if botao.get_attribute("aria-label") == "Início":
         botao.click()
         break
 
 
 
 # selecionar uma aba
-abas = navegador.window_handles # Declara todas as abas abertas
+# abas = navegador.window_handles # Declara todas as abas abertas
 
-# navegar pelas abas
-navegador.switch_to.window(abas[1])
+# # navegar pelas abas
+# navegador.switch_to.window(abas[1])
 
-# navegar para um site diferente
-navegador.get("https://www.youtube.com/")
+# # navegar para um site diferente
+# navegador.get("https://www.youtube.com/")
 
-# escrever em algum campo ou formulário
-navegador.find_element(By.ID, "P101_USERNAME").send_keys("CARLOS.ALMEIDA")
-navegador.find_element(By.ID, "P101_PASSWORD_CONTAINER").send_keys("Deborah")
+# # escrever em algum campo ou formulário
+# navegador.find_element(By.ID, "P101_USERNAME").send_keys("CARLOS.ALMEIDA")
+# navegador.find_element(By.ID, "P101_PASSWORD_CONTAINER").send_keys("Deborah")
 
-time.sleep(10)
+time.sleep(200)
--- Selenium/main.py
+++ Selenium/main.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 
 # importar o navegador
 navegador = webdriver.Chrome()
+
+wait = WebDriverWait(navegador, 10) # Pausa dinâmica que espera 10 seg. até que uma ação seja executada, caso contrário dará ERRO.
 
 # acessar um site
 navegador.get("https://app.simplificagestao.com.br/ords/r/api/simplifica/login_desktop")
@@ -13,10 +18,54 @@ navegador.maximize_window()
 
 # escrever em algum campo ou formulário
 navegador.find_element(By.ID, "P101_USERNAME").send_keys("CARLOS.ALMEIDA") # login
-navegador.find_element(By.ID, "P101_PASSWORD_CONTAINER").send_keys("Deborah") # senha
+navegador.find_element(By.ID, "P101_PASSWORD").send_keys("Deborah") # senha
 
 botao_logar = navegador.find_element(By.ID, "P101_LOGIN")
 
 # Clicar 
 botao_logar.click()
+
+
+
+# Aba de Pesquisa
+abaPesquisa = navegador.find_element(By.ID, "t_TreeNav_3")
+abaPesquisa.click()
+
+# Razão Social no Sistema
+razaoSocial = navegador.find_element(By.ID, "P146_NOME")
+razaoSocial.send_keys(Keys.CONTROL, "v")
+
+# Botão de Pesquisa
+botaoPesquisa = navegador.find_element(By.ID, "B2499226662932143290")
+botaoPesquisa.click()
+
+# Seleciona a Primeira "lupinha" para Abrir a Empresa
+lupinhas = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'td[headers="lista_pessoa_LINK"] a')))
+lupinhas[0].click()
+
+# Selecionar o Campo de AÇÕES
+acoes = wait.until(EC.presence_of_element_located((By.ID, "B2441611845714781178")))
+acoes.click()
+
+# Selecionar o botão de consultor
+trocaConsultor = wait.until(EC.presence_of_element_located((By.ID, "B4175794322982668587")))
+trocaConsultor.click()
+
+# Selecionar o label de consultores
+labelConsultor = wait.until(EC.presence_of_all_elements_located((By.ID, "P578_CONSULTOR_NOVO_lov_btn")))
+labelConsultor.click()
+
+# Selecionar o novo Consultor
+jorge = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "li[data-id='JORGE.FILHO']")))
+jorge.click()
+
+# helaine = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "li[data-id='HELAINE.BRITO']")))
+# helaine.click()
+# augusto = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "li[data-id='AUGUSTO.MATIAS']")))
+# augusto.click()
+
+# Confirmar a troca de consultor
+confirmarTroca = wait.until(EC.presence_of_all_elements_located((By.ID, "B2496065396947439746")))
+confirmarTroca.click()
+
 time.sleep(10)
